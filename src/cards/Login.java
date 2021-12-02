ec162d25aa2b129fab7ae11a3ccb61774e4a2be4
Aishwarea Gupthan <aishwareagupthan@gmail.com>
2021-12-02 16:10:22 +0530

    Added Buttons And an Icon
    Manager login, Teacher Login and View Stats Button and Census Logo Icon added as an label

--- src/cards/Login.java
+++ src/cards/Login.java
@@ -6,14 +6,29 @@ import java.awt.event.*;
 
 public class Login extends JPanel {
     public Login() {
-        JButton nextButton = new JButton("Login");
+    	ImageIcon i = new ImageIcon("http://www.dataforall.org/dashboard/censusinfoindia_pca/img/logo.png");
+    	JLabel label = new JLabel(i,SwingConstants.HORIZONTAL);
+        JButton nextButton = new JButton("View Stats");
+        JButton TButton = new JButton("Teacher Dashboard Login");
+        JButton MButton = new JButton("Manager Dashboard Login");
         nextButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                Master.goTo("TeacherDashboard");
-                System.out.println("");
+            	Object source = e.getSource();
+            	if(source == TButton) {
+            		Master.goTo("Teacher Dashboard");
+            	}
+            	else if(source == MButton) {
+            		Master.goTo("Master Dashboard");
+            	}
+            	else if(source == nextButton) {
+            		Master.goTo("Stats View");
+            	}
             }
         });
+        add(label);
         add(nextButton);
+        add(TButton);
+        add(MButton);
     }
 }
