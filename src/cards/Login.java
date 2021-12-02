9458cfc8b950ed30b16da92a8788a9f1c2d5ba52
Aishwarea Gupthan <aishwareagupthan@gmail.com>
2021-12-03 00:07:51 +0530

    Alignments And Rename Button
    Aligning the buttons and icon and renamed some of the buttons

--- src/cards/Login.java
+++ src/cards/Login.java
@@ -6,29 +6,34 @@ import java.awt.event.*;
 
 public class Login extends JPanel {
     public Login() {
+    	JPanel panel = new JPanel();
     	ImageIcon i = new ImageIcon(Master.class.getResource("/cards/logo.png"));
     	JLabel label = new JLabel(i,SwingConstants.HORIZONTAL);
         JButton nextButton = new JButton("View Stats");
         JButton TButton = new JButton("Teacher Dashboard Login");
         JButton MButton = new JButton("Manager Dashboard Login");
+        
         nextButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
             	Object source = e.getSource();
             	if(source == TButton) {
-            		Master.goTo("Teacher Dashboard");
+            		Master.goTo("TeacherDashboard");
             	}
             	else if(source == MButton) {
-            		Master.goTo("Master Dashboard");
+            		Master.goTo("ManagerDashboard");
             	}
             	else if(source == nextButton) {
-            		Master.goTo("Stats View");
+            		Master.goTo("Statstics");
             	}
             }
         });
+        
         add(label);
-        add(nextButton);
-        add(TButton);
-        add(MButton);
+        panel.add(nextButton);
+        panel.add(TButton);
+        panel.add(MButton);
+        add(panel);
+        
     }
 }
--- src/cards/TeacherDashboard.java
+++ src/cards/TeacherDashboard.java
@@ -6,7 +6,7 @@ import java.awt.event.*;
 
 public class TeacherDashboard extends JPanel {
     public TeacherDashboard() {
-        JButton previousButton = new JButton("Teacher Dashboard");
+        JButton previousButton = new JButton("TeacherDashboard");
         previousButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
