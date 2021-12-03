05538b50a5aca2f537c779aa66e049be3e76a39d
Aishwarea Gupthan <aishwareagupthan@gmail.com>
2021-12-03 14:47:28 +0530

    Button Activation and vertical alignment of buttons
    add action listener

--- src/cards/Login.java
+++ src/cards/Login.java
@@ -4,34 +4,41 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
-public class Login extends JPanel {
+public class Login extends JPanel implements ActionListener{
+	JPanel panel = new JPanel();
+	ImageIcon i = new ImageIcon(Master.class.getResource("/cards/logo.png"));
+	JLabel label = new JLabel(i,SwingConstants.HORIZONTAL);
+    JButton nextButton = new JButton("View Popolation Statstics");
+    JButton TButton = new JButton("Teacher Dashboard Login");
+    JButton MButton = new JButton("Manager Dashboard Login");
     public Login() {
-    	JPanel panel = new JPanel();
-    	ImageIcon i = new ImageIcon(Master.class.getResource("/cards/logo.png"));
-    	JLabel label = new JLabel(i,SwingConstants.HORIZONTAL);
-        JButton nextButton = new JButton("View Stats");
-        JButton TButton = new JButton("Teacher Dashboard Login");
-        JButton MButton = new JButton("Manager Dashboard Login");
-        nextButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-            	Object source = e.getSource();
-            	if(source == TButton) {
-            		Master.goTo("Teacher Dashboard");
-            	}
-            	else if(source == MButton) {
-            		Master.goTo("Manager Dashboard");
-            	}
-            	else if(source == nextButton) {
-            		Master.goTo("View Statstics");
-            	}
-            }
-        });
-        
+    	setBackground(Color.white);
         add(label);
+        add(Box.createRigidArea(new Dimension(500,25)));
         panel.add(nextButton);
         panel.add(TButton);
         panel.add(MButton);
+        panel.setAlignmentX(Component.CENTER_ALIGNMENT);
+        panel.setPreferredSize(new Dimension(200,125));
+        panel.setBackground(Color.white);
+        panel.setBorder(BorderFactory.createTitledBorder("Main Menu"));
         add(panel);
+        
+        nextButton.addActionListener(this);
+        TButton.addActionListener(this);
+        MButton.addActionListener(this);
+    }
+    public void actionPerformed(ActionEvent e) {
+    	Object source = e.getSource();
+    	if(source == TButton) {
+    		
+    		Master.goTo("Teacher Dashboard");
+    	}
+    	else if(source == MButton) {
+    		Master.goTo("Manager Dashboard");
+    	}
+    	else if(source == nextButton) {
+    		Master.goTo("View Statstics");
+    	}
     }
 }
